b8e62704f9ca87d680edbf74c9301f8a6ab6a0cf
jbm950 <jmilam343@gmail.com>
2015-06-19 11:10:37 -0500

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='pygame_toolbox',
+      version='0.1',
+      license='MIT',
+      description='Tools to help with game development using pygame',
+      author='James Milam',
+      author_email='jmilam343@gmail.com',
+      url='https://github.com/jbm950/pygame_toolbox',
+      packages=find_packages(),
+      keywords='pygame'
+      )
